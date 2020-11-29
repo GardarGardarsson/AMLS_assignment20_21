8b769b8456389f05d439c1719b73ba41680dd834
GardarGardarsson <gardar.gardarsson.20@ucl.ac.uk>
2020-11-29 09:28:33 +0000

    Tidying up branch after last nights mix up due to a file exceeding the 100 MB limit

--- code/import_data.py
+++ code/import_data.py
@@ -39,28 +39,32 @@ plt.rc('legend', fontsize=SMALL)    # Legend fontsize
 plt.rc('figure', titlesize=HUGE)    # Fontsize of the figure title
 
 """
-I M A G E   I M P O R T 
+I M A G E   I M P O R T
 """
 
-def importImages(img_path,surpress=False):
+def importImages(img_path, img_type='.jpg', surpress=False):
     
     print('Importing image data...')
     
     # Define a list to hold image data
     X = [] # Our feature vector
     
+    # Navigate to the subfolder that was handed to us
+    dir_path = os.getcwd()
+    full_path = dir_path + img_path
+    
     # Let's perform this action for every single image in our directory
-    for i in range(len(os.listdir(img_path))):
+    for i in range(len(os.listdir(full_path))):
         # We'll devise a filename to keep things in order
-        filename = str(i) + '.jpg'
+        filename = str(i) + img_type
         # We append each 178 x 218 px and 3 channel (RGB) to the list
-        X.append(imageio.imread(img_path + filename))
+        X.append(imageio.imread(full_path + filename))
     
     # We'll now convert our list to a numpy array
     X = np.array(X)
     
     # As there are 5000 images, the dimensions of our array should be 5000 x 3 x 178 x 218
-    # We can return the size of the array to verify that all images were read 
+    # We can return the size of the array to verify that all images were read
     # We may choose to surpress outputs
     if not surpress:
         print("Image data is stored in numpy array 'X' of size: {}".format(X.shape))
@@ -70,7 +74,7 @@ def importImages(img_path,surpress=False):
     return X
 
 """
-L A B E L   I M P O R T 
+L A B E L   I M P O R T
 """
 
 def importLabels(label_path,surpress=False):
@@ -80,14 +84,18 @@ def importLabels(label_path,surpress=False):
     # Define a list to hold label data
     y = [] # Label vector
     
+    # Navigate to the subfolder that was handed to us
+    dir_path = os.getcwd()
+    full_path = dir_path + label_path
+    
     # Scan for .csv's on label_path
-    entriesOnPath = os.listdir(label_path)
+    entriesOnPath = os.listdir(full_path)
     
     # Extract the name of the label file
     label_file = [file for file in entriesOnPath if file.endswith(".csv")]
     
     # Let us now load the labels from the .csv file
-    y = pd.read_csv(label_path + label_file[0],sep="\t")
+    y = pd.read_csv(full_path + label_file[0],sep="\t")
     
     # We may choose to surpress outputs
     if not surpress:
@@ -101,55 +109,113 @@ def importLabels(label_path,surpress=False):
 I M P O R T   D A T A   &   V I S U A L I S E
 """
 
-def dataImport(img_path,label_path,surpress=False,return_img_indices=False):    
+def dataImport(img_path,label_path,img_type='.jpg',task='A',surpress=False,return_img_indices=False):
     
     # We load image and label data to vectors X and y respectively
-    X = importImages(img_path,surpress)
+    X = importImages(img_path,img_type,surpress)
     y = importLabels(label_path,surpress)
-    
-    # Let's print a couple of random images from our imported data with corresponding labels
-    random_img = np.random.randint(5000, size=(2,3))
-    
-    # We may choose to surpress outputs in our function call
-    if not surpress:
-        # Displaying random image data...
-        print('Displaying random images and corresponding labels from set...')
+
+    """
+    P L O T   T A S K   A
+    """
+    # Display images from the celebrity dataset
+    if task == 'A':
         
-        # Define a tuple to iterate over
-        row, col = random_img.shape
+        # Let's print a couple of random images from our imported data with corresponding labels
+        random_img = np.random.randint(5000, size=(2,3))
         
-        # Define layout and size of subplot
-        fig, ax = plt.subplots(nrows=row, ncols=col, figsize=(12,8))
-        
-        # Let's populate our 2x3 subplot
-        for i in range(row) :
-            for j in range(col):
-                # We extract a 4D array from our image library
-                extract = X[(random_img[i,j]),:,:,:]
-                # Get rid of the 4th dimension, i.e. squeeze back to 3D
-                img = np.squeeze(extract)
+        # We may choose to surpress outputs in our function call
+        if not surpress:
                 
-                # We can then plot the image using matplotlib's .imshow() function
-                ax[i,j].imshow(img)
-                # Turn off plot axes
-                ax[i,j].axis("off")
+                # Displaying random image data...
+                print('Displaying random images and corresponding labels from set...')
                 
-                # Set the title of each image as the corresponding labels
-                gender = y.loc[random_img[i,j],'gender']
-                smiling = y.loc[random_img[i,j],'smiling']
-                title = "Gender: {} \n Smiling: {}".format(("Female" if gender == -1 else "Male"),
-                                                          ("No" if smiling == -1 else "Yes"))
-                ax[i,j].set_title(title)
+                # Define a tuple to iterate over
+                row, col = random_img.shape
                 
-        # Set tight layout and display the plot
-        plt.suptitle('Randomly chosen images and corresponding labels from set')
-        plt.tight_layout()
-        plt.show()
-        
-    # If outputs were surpressed, do nothing
-    else:
-        pass
+                # Define layout and size of subplot
+                fig, ax = plt.subplots(nrows=row, ncols=col, figsize=(12,8))
+                
+                # Let's populate our 2x3 subplot
+                for i in range(row) :
+                    for j in range(col):
+                        # We extract a 4D array from our image library
+                        extract = X[(random_img[i,j]),:,:,:]
+                        # Get rid of the 4th dimension, i.e. squeeze back to 3D
+                        img = np.squeeze(extract)
+                        
+                        # We can then plot the image using matplotlib's .imshow() function
+                        ax[i,j].imshow(img)
+                        # Turn off plot axes
+                        ax[i,j].axis("off")
+                        
+                        # Set the title of each image as the corresponding labels
+                        gender = y.loc[random_img[i,j],'gender']
+                        smiling = y.loc[random_img[i,j],'smiling']
+                        title = "Gender: {} \n Smiling: {}".format(("Female" if gender == -1 else "Male"),
+                                                                  ("No" if smiling == -1 else "Yes"))
+                        ax[i,j].set_title(title)
+                        
+                # Set tight layout and display the plot
+                plt.suptitle('Randomly chosen images and corresponding labels from set')
+                plt.tight_layout()
+                plt.show()
+  
+        # If outputs were surpressed, do nothing
+        else:
+            pass
     
+    """
+    P L O T   T A S K   B
+    """
+    
+    # If the task is 'B' or other, we display images from the cartoon dataset
+    if task == 'B':
+        
+        # Let's print a couple of random images from our imported data with corresponding labels
+        random_img = np.random.randint(10000, size=(2,3))
+        
+        # We may choose to surpress outputs in our function call
+        if not surpress:
+            
+             # Displaying random image data...
+            print('Displaying random images and corresponding labels from set...')
+            
+            # Define a tuple to iterate over
+            row, col = random_img.shape
+            
+            # Define layout and size of subplot
+            fig, ax = plt.subplots(nrows=row, ncols=col, figsize=(12,8))
+            
+            # Let's populate our 2x3 subplot
+            for i in range(row) :
+                for j in range(col):
+                    # We extract a 4D array from our image library
+                    extract = X[(random_img[i,j]),:,:,:]
+                    # Get rid of the 4th dimension, i.e. squeeze back to 3D
+                    img = np.squeeze(extract)
+                    
+                    # We can then plot the image using matplotlib's .imshow() function
+                    ax[i,j].imshow(img)
+                    # Turn off plot axes
+                    ax[i,j].axis("off")
+                    
+                    # Set the title of each image as the corresponding labels
+                    face_shape = y.loc[random_img[i,j],'face_shape']
+                    eye_color = y.loc[random_img[i,j],'eye_color']
+                    title = "Face shape: {} \n Eye colour: {}".format((face_shape),(eye_color))
+                    ax[i,j].set_title(title)
+                    
+            # Set tight layout and display the plot
+            plt.suptitle('Randomly chosen images and corresponding labels from set')
+            plt.tight_layout()
+            plt.show()
+            
+        # If outputs were surpressed, do nothing
+        else:
+            pass
+        
+        
     # If user has requested to retrieve indices used to show random images we return them with X and y
     if return_img_indices:
         return X,y,random_img
--- code/main.py
+++ code/main.py
@@ -1,3 +1,8 @@
+# %% Import libraries...
+"""
+L I B R A R Y   I M P O R T
+"""
+
 # To modularise the program, we need to add the current directory to sys path
 import os
 import sys
@@ -10,26 +15,27 @@ sys.path.append(currentPath)
 
 # Now we can import our own modules into our script.
 import import_data as ds
-import split_dataset as sd 
+import split_dataset as sd
 import pre_processing as prp
 
 # numpy for enhanced mathematical support
 import numpy as np
 # Matplotlib for visualisation
 import matplotlib.pyplot as plt
+# Pandas dataframes for enhanced data storage
+import pandas as pd
 
 
-
+# %% Load and split data...
 if __name__ == '__main__':
     
+    """
+    L O A D   D A T A
+    """
     # Define a path to the data - REMEMBER TO RESET THIS BEFORE TURNING IN
-    img_path = "/Users/gardar/Documents/UCL/ELEC0134 MLS-I Applied Machine Learning Systems/Assignments/dataset_AMLS_20-21/celeba/img/"
-    label_path = "/Users/gardar/Documents/UCL/ELEC0134 MLS-I Applied Machine Learning Systems/Assignments/dataset_AMLS_20-21/celeba/"
+    img_path = "/Datasets/celeba/img/"
+    label_path = "/Datasets/celeba/"
     
-    # %%
-    """
-    L O A D   D A T A 
-    """
     # Load image and label data with the novel 'import_data' module
     X , y , random_img = ds.dataImport(img_path,label_path,surpress=False,return_img_indices=True)
     
@@ -39,54 +45,285 @@ if __name__ == '__main__':
     # Split dataset into train-, validation- and test folds
     Xtrain,Xval,Xtest,ytrain,yval,ytest = sd.split_dataset(X,y,test_size=0.2,val_size=0.2,surpress=False)
     
-    # %%
+    # %% Augment data...
     """
     D A T A   A U G M E N T A T I O N
     """
     
+    # We may want to perform data augmentation ...
+    # For e.g. flips, mirroring, concealment.
+    # I'll leave this cell empty for later implementation.
     
     
-    
-    #%%
+    #%% Crop images...
     """
-    P R E - P R O C E S S :   F A C E   E N C O D I N G S
+    P R E - P R O C E S S :   C R O P
     """
     
-    import face_recognition
+    def crop(img_arr):
+        # Initialise an empty array that can accomodate as many instances
+        # as the number of images we wish to crop, in the required final dimensions
+        cropped = np.empty((img_arr.shape[0],
+                            img_arr.shape[1]-40, # Subtract 40px from the height
+                            img_arr.shape[2],
+                            img_arr.shape[3]),int)
+        
+        # For each of the images provided
+        for i,img in enumerate(img_arr):
+            # We store a cropped version, that crops a 20 px banner from the
+            # top and bottom of the image
+            cropped[i] = img[20:20+178]
+            
+        return cropped
     
-    Xtrain_FE = []
+    Xtrain_C = crop(Xtrain)
+    Xval_C = crop(Xval)
+    Xtest_C = crop(Xtest)
     
-    for img in Xtrain:
-        Xtrain_FE.append(face_recognition.face_encodings(img))
+    #%% Grayscale images...
+    """
+    P R E - P R O C E S S :   G R A Y S C A L E
+    """
     
-    Xtrain_FE = np.array(Xtrain_FE)
+    # Reduce the RGB channels to a singular gray channel using Rec. 601 encoding
+    Xtrain_gry = prp.reduceRGB(Xtrain_C).astype(np.uint16)
+    Xval_gry = prp.reduceRGB(Xval_C).astype(np.uint16)
+    Xtest_gry = prp.reduceRGB(Xtest_C).astype(np.uint16)
+    
+    #%% Perform histogram equalization...
+    """
+    P R E - P R O C E S S :   H I S T O G R A M   E Q U A L I S A T I O N
+    """
+    
+    print("Checkpoint: Start Histogram Equalisation")
+    
+    # To perform Contrast Limited Adaptive Histogram Equalisation (CLAHE)
+    # we import a scikit-image package
+    from skimage.exposure import equalize_adapthist
+    
+    # We equalize all the image data using an adaptive contrast equaliser
+    Xtrain_eq = equalize_adapthist(Xtrain_gry)
+    Xval_eq = equalize_adapthist(Xval_gry)
+    Xtest_eq = equalize_adapthist(Xtest_gry)
+    
+    print("Checkpoint: End Histogram Equalisation")
+    
+    #%% Edge detection with Sobel-Feldman operator...
+    """
+    P R E - P R O C E S S :   S O B E L
+    """
+    
+    # We now proceed by processing the images with a Sobel filter
+    # The Sobel-Feldman operator provides powerful means of edge detection
+    # especially, on equalised images
+    from scipy import ndimage
+    
+    def sobel(img_arr, surpress=False):
+        
+        # The sobel function assumes the dimensions of the image array it is passed
+        sobel = np.empty(img_arr.shape)
+        
+        for i,img in enumerate(img_arr):
+            
+            # x-directional SOBEL gradient operator
+            # mode = 'constant' fills values beyond edges with a constant value cval
+            s_x = ndimage.sobel(img, axis=0, mode='constant',cval = 0.0)
+            
+            # y-directional SOBEL gradient operator
+            s_y = ndimage.sobel(img, axis=1, mode='constant',cval = 0.0)
+            
+            # Our combined filter response is the hypotenuse of the x- and y-components
+            sobel[i] = np.hypot(s_x, s_y)
+        
+        return sobel
+    
+    Xtrain_sob = sobel(Xtrain_eq)
+    Xval_sob = sobel(Xval_eq)
+    Xtest_sob = sobel(Xtest_eq)
+    
+    # %% Flatten image data to 1D feature vector...
+    """
+    P R E - P R O C E S S :   F L A T T E N   T O  1 D - F E A T U R E   V E C T O R
+    """
+    
+    def flatten(img_arr):
+        N,h,w = img_arr.shape
+        flattened = np.reshape(img_arr,(N,(h*w)))
+        return flattened
+    
+    Xtrain_flat = flatten(Xtrain_sob)
+    Xval_flat = flatten(Xval_sob)
+    Xtest_flat = flatten(Xtest_sob)
+    
+    # %%
+    """
+    P R E - P R O C E S S :   S C A L E   D A T A
+    """
+    
+    # Import a standard scaler
+    from sklearn.preprocessing import StandardScaler
+    
+    # Fit the scaler to the training data
+    scaler = StandardScaler().fit(Xtrain_flat)
+    
+    # Apply the transformation to the training, validation and testing data
+    Xtrain_scl  =   scaler.transform(Xtrain_flat)
+    Xval_scl    =   scaler.transform(Xval_flat)
+    Xtest_scl   =   scaler.transform(Xtest_flat)
+    
+    
+    
+    # %% Prepare label data...
+    """
+    P R E P A R E   L A B E L   D A T A
+    """
+    
+    # The keras utilities module contains a function to transform data to
+    # one hot vector
+    from keras.utils import to_categorical
+    
+    def to_one_hot_vector(label_data,category):
+        
+        # Transform a column of a label dataframe to one hot vector
+        labels = label_data.loc[:,category].copy()
+        one_hot_vec = to_categorical(labels)
+        
+        return one_hot_vec
+    
+    def to_binary(label_data,category):
+    
+        # Transform -1 | 1 categorised data to 0 | 1 values
+        labels = label_data.loc[:,category].copy()
+        labels[labels<0] = 0
+        
+        return labels
+    
+    # Transform the 'gender' column of the ytrain-, validation and test
+    # labels to one hot vector
+    ytrain_A1 =  to_binary(ytrain,'gender')
+    yval_A1   =  to_binary(yval,'gender')
+    ytest_A1  =  to_binary(ytest,'gender')
+    
+    # Transform the 'smiling' column of the ytrain-, validation and test
+    # labels to one hot vector
+    ytrain_A2 =  to_one_hot_vector(ytrain,'smiling')
+    yval_A2   =  to_one_hot_vector(yval,'smiling')
+    ytest_A2  =  to_one_hot_vector(ytest,'smiling')
+    
+    # %% Perform Cross-Validation...
+    """
+    B U I L D   P I P E L I N E :   P C A + S V M
+    """
 
+    # Let us now import an unsupervised classifier for the
+    # dimensionality reduction of the data, namely that of PCA
+    from sklearn.decomposition import PCA
+    
+    # We will also use the SVM classifier of the scikit-learn library
+    from sklearn.svm import SVC
+    
+    # To optimise the number of components, and parameters of our SVM,
+    # we shall construct a pipeline, and perform a gridsearch to find
+    # both the optimal number of principal components and hyperparameters for the SVM.
+    from sklearn.pipeline import Pipeline
+    from sklearn.model_selection import GridSearchCV
+    
+    # So we don't have to perform the CV numerous times
+    import joblib
+    
+    # If we don't have a record of previous cross-validation
+    if not os.path.isfile('CV_object.pkl'):
+            
+        # We initialise an empty PCA model
+        pca = PCA()
+        
+        # Use a linear SVC
+        svm = SVC()
+        
+        # Combine PCA and SVC to a pipeline
+        pipe = Pipeline(steps=[('pca', pca), ('svm', svm)])
+        
+        # Number of Principal components to try
+        n_components = [100,500,1000]
+        
+        # Parameters to try for the SVM and PCA
+        params_grid = {
+        'svm__C': [0.01, 0.1, 1],
+        'svm__kernel': ['linear','rbf','poly'],
+        'svm__degree' : [2],
+        'svm__gamma': [0.9, 0.5, 0.1],
+        'pca__n_components': n_components,
+        }
+        
+        # Initialise the gridsearch cross validation
+        estimator = GridSearchCV(pipe,                      # Use the PCA + SVM pipeline
+                                 params_grid,               # For these SVM hyperparams (and components)
+                                 scoring='accuracy',        # Score on validation accuracy
+                                 n_jobs = 2,                # Use 2 processors
+                                 return_train_score=True,   # Record the scores for later use
+                                 verbose = 10)              # Print the process as it runs
+        
+        # Fit the model on the training data
+        estimator.fit(Xtrain_scl, ytrain_A1)
+        
+        # Output the best parameters and the score they achieved
+        print(estimator.best_params_, estimator.best_score_)
+        
+        # Pickle the results for later consumption
+        joblib.dump(estimator, 'CV_object.pkl')
+        
     # %%
-    print(Xtrain_FE)
+    """
+    T A S K   B :   C A R T O O N   D A T A S E T
+    """
+    
     
     # %%
     """
-    P R E - P R O C E S S   D A T A   P C A 
+    L O A D   D A T A
     """
-    # Center images on mean
-    Xtrain,trainMean = prp.imgProcessing(Xtrain,surpress=False)
-    Xval,trainMean = prp.imgProcessing(Xval,surpress=True,mean=trainMean)
-    Xtest,trainMean = prp.imgProcessing(Xtest,surpress=True,mean=trainMean)
+    # Define a path to the data - REMEMBER TO RESET THIS BEFORE TURNING IN
+    img_path = "/Datasets/cartoon_set/img/"
+    label_path = "/Datasets/cartoon_set/"
     
-    # Perform PCA on test data to obtain eigenvalues and vectors
-    Sigma,WT = prp.PCA_w_SVD(Xtrain,surpress=False)
-    
-    """
-    V I S U A L I S E   E I G E N F A C E S 
-    """
-    # Let's check what these spooky eigenfaces look like
-    prp.showEigenfaces(WT,X)
+    # Load image and label data with the novel 'import_data' module
+    X , y , random_img = ds.dataImport(img_path,
+                                       label_path,
+                                       img_type='.png',
+                                       task='B',
+                                       surpress=False,
+                                       return_img_indices=True)
     
     """
-    F I T   V A L I D A T I O N   A N D   T E S T   T O   P C A 
+    S P L I T   D A T A
     """
-    Xtrain = prp.fitPCA(WT,Xtrain,n_components=20)
-    Xval = prp.fitPCA(WT,Xval,n_components=20)
-    Xtest = prp.fitPCA(WT,Xtest,n_components=20)
+    # Split dataset into train-, validation- and test folds
+    Xtrain,Xval,Xtest,ytrain,yval,ytest = sd.split_dataset(X,y,test_size=0.2,val_size=0.2,surpress=True)
     
     
+    # %% Crop images...
+    """
+    P R E - P R O C E S S :   C R O P
+    """
+    
+    def crop(img_arr):
+        # Initialise an empty array that can accomodate as many instances
+        # as the number of images we wish to crop, in the required final dimensions
+        cropped = np.empty((img_arr.shape[0],
+                            img_arr.shape[1]-425, # Subtract 40px from the height
+                            img_arr.shape[2]-300,
+                            img_arr.shape[3]),int)
+        
+        # For each of the images provided
+        for i,img in enumerate(img_arr):
+            # We store a cropped version, that crops a 20 px banner from the
+            # top and bottom of the image
+            cropped[i] = img[225:225+75,150:150+200]
+            
+        return cropped
+    
+    Xtrain_C = crop(Xtrain[:300])
+    
+    plt.imshow(Xtrain_C[101])
+    print(Xtrain_C[101].shape)
+    # %%
